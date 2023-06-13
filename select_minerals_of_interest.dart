bb54556e51ee9eecb2154225a9ef57bb9e4476ed
nuilewis <lewisnuikweh@gmail.com>
2023-06-13 08:51:46 +0100

    Added cleaning method to clean list, and minerals of interest.

--- select_minerals_of_interest.dart
+++ select_minerals_of_interest.dart
@@ -4,51 +4,95 @@ void main() async {
   final List<String> mineralList = [
     "quartz",
     "calcite",
+    "bauxite",
+    "copper",
+    "feldspar",
+    "silver",
+    "gold",
+    "gneiss",
+    "granite",
+    "basalt",
   ];
 
-  //final File csvFile = File("img_url_list_converted.csv");
+  final File csvFile = File("img_url_list_converted.csv");
 
-  final File csvFile = File("img_list_test.csv");
+  //final File csvFile = File("img_list_test.csv");
   final String csvfileAsString = await csvFile.readAsString().whenComplete(
     () {
       print("imported csv file");
     },
   );
 
-  List<String> selectedMinerals = selectMineralOfInterest(
-      InterestedMineralsList: mineralList, csv: csvfileAsString);
+  ///Optimize the CSV File and remove entries with multiple minerals
 
-  ///Writing the file to disk
+  List<String> cleanedMineralList = cleanDataSet(csvfileAsString);
+
+  File cleanMinerals = File("selected_minerals/cleaned_mineral_list.csv");
+
+  await cleanMinerals.writeAsString(cleanedMineralList.join());
+
+  ///Select Minerals of Interest
+
+  List<String> selectedMinerals = selectMineralOfInterest(
+      InterestedMineralsList: mineralList, csv: cleanedMineralList);
+
+  ///Writing the output file to disk
 
   File outPutCSV = File("selected_minerals/selected_minerals.csv");
 
   await outPutCSV.writeAsString(selectedMinerals.join());
 }
 
+//-------------------------------
 List<String> selectMineralOfInterest({
   required List<String> InterestedMineralsList,
-  required String csv,
+  required List<String> csv,
 }) {
   //Conver the csv into a list
 
   List<String> listofJoinedMinerals = [];
 
-  List<String> splitList =
-      csv.split('\n').toList(); // Splits th list at each new line
-
-  for (int i = 0; i < splitList.length; i++) {
+  for (int i = 0; i < csv.length; i++) {
     //loops through and Check if the mineral is among the list of out interested minerals then add to the list
 
     for (String mineralName in InterestedMineralsList) {
-      if (splitList[i].contains(mineralName)) {
+      if (csv[i].contains(mineralName)) {
         listofJoinedMinerals
-            .add(splitList[i]); //Adds our mineral of interest to a new list
+            .add(csv[i]); //Adds our mineral of interest to a new list
       }
     }
   }
-  print("final list with minerals of interest");
-  print(listofJoinedMinerals.toList());
 
-  //Write the list to a csv file
+  print("selected our minerals of interest");
   return listofJoinedMinerals;
+}
+
+//--------------------------------------------------------
+List<String> cleanDataSet(String csv) {
+  List<String> csvList =
+      csv.split('\n').toList(); // Splits the list at each new line
+
+  List<String> chosenMinerals = [];
+  for (int i = 0; i < csvList.length; i++) {
+    String entry = csvList[i];
+    entry.trim();
+
+    ///remove any extra space at the endg that makes it think there is a
+    /// new column this include the trailing comma, a space, and a "new line" charcter
+if(entry.length>3){
+    entry = entry.substring(0,
+        entry.length - 3); //Remove the comma, space, and faux space at the end
+}
+
+
+    if (entry.split(' ').length == 2) {
+      ///Take an entry, split it into the different words. If it has less than 2 items in the list,
+      ///then its an image with only a single mineral type and lable, so it should be used.
+      chosenMinerals.add('${entry}\n');
+    }
+  }
+
+  print("cleaned up our list");
+
+  return chosenMinerals;
 }
